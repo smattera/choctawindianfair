f1872afb329cf95212da0805d9a6b8fea2ae47ac
Sonny Mattera <smattera@hotmail.com>
2024-03-26 13:56:16 -0500

    Update Pricing and Diamond SVG

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -4,10 +4,10 @@ export default function Pricing() {
       <div className="mx-auto max-w-7xl px-6 pb-96 pt-24 text-center sm:pt-32 lg:px-8">
         <div className="mx-auto max-w-4xl">
           <h2 className="text-base font-semibold leading-7 text-amber-600 dark:text-amber-200">
-            Pricing
+            Pre-Sale Starts June 11, 2024
           </h2>
           <p className="mt-2 text-4xl font-bold tracking-tight text-foreground sm:text-5xl">
-            Pricing plans for teams of&nbsp;all&nbsp;sizes
+            Ticket Prices
           </p>
         </div>
       </div>
