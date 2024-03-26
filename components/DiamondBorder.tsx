165bdbdee38108cee2e3a6a9dd059305e0f8df1c
Sonny Mattera <smattera@hotmail.com>
2024-03-26 16:32:18 -0500

    Refactor DiamondBorder component to use a nested div for background color

--- components/DiamondBorder.tsx
+++ components/DiamondBorder.tsx
@@ -4,7 +4,9 @@ export default function DiamondBorder() {
       <div className="h-1 w-full bg-background" />
       <div className="h-1.5 w-full bg-[#c4b166]" />
       <div className="h-1 w-full bg-background" />
-      <div className="h-10 w-full bg-[url('/img/svg/diamond.svg')]" />
+      <div className="h-10 w-full bg-[#38363c]">
+        <div className="h-10 w-full bg-[url('/img/svg/diamond.svg')]" />
+      </div>
       <div className="h-1 w-full bg-background" />
       <div className="h-1.5 w-full bg-[#c09c2f]" />
       <div className="h-1 w-full bg-background" />
