b177c901b245ee322bdb4a925a4e7427eb2afcfb
Sonny Mattera <smattera@hotmail.com>
2024-03-26 09:09:28 -0500

    Update BackgroundVid component styling

--- components/BackgroundVid.tsx
+++ components/BackgroundVid.tsx
@@ -76,7 +76,7 @@ export default function BackgroundVid() {
   }, [currentVideoIndex]);
 
   return (
-    <div className="relative h-3/4 w-full overflow-hidden bg-black sm:h-2/3">
+    <div className="relative h-svh w-full overflow-hidden bg-black">
       <video
         autoPlay
         muted
