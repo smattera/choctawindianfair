1555a60ad9e7ba9c4e4991ac8f3bf916fdb2f8ef
Sonny Mattera <smattera@hotmail.com>
2024-03-28 09:06:38 -0500

    Refactor DiamondBorder component and remove unused SVG file

--- components/DiamondBorder.tsx
+++ components/DiamondBorder.tsx
@@ -4,9 +4,13 @@ export default function DiamondBorder() {
       <div className="h-1 w-full bg-background" />
       <div className="h-1.5 w-full bg-[#c4b166]" />
       <div className="h-1 w-full bg-background" />
-      <div className="h-10 w-full bg-[#38363c]">
-        <div className="h-10 w-full bg-[url('/img/svg/diamond.svg')]" />
-      </div>
+      <div
+        className="h-10 w-full bg-[#38363c]"
+        style={{
+          backgroundImage: `url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 20" fill="%2392864f"%3E%3Cpolygon points="13,1 25,10 13,19 1,10" /%3E%3C/svg%3E')`,
+          backgroundRepeat: "repeat-x",
+        }}
+      />
       <div className="h-1 w-full bg-background" />
       <div className="h-1.5 w-full bg-[#c09c2f]" />
       <div className="h-1 w-full bg-background" />
