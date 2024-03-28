0764c6e0d81366ec4eb80c9cc8603a9e4eb1e00e
Sonny Mattera <smattera@hotmail.com>
2024-03-28 09:34:46 -0500

    Update DiamondBorder component background image repeat

--- components/DiamondBorder.tsx
+++ components/DiamondBorder.tsx
@@ -5,10 +5,9 @@ export default function DiamondBorder() {
       <div className="h-1.5 w-full bg-[#c4b166]" />
       <div className="h-1 w-full bg-background" />
       <div
-        className="h-10 w-full bg-[#38363c]"
+        className="h-10 w-full bg-[#38363c] bg-repeat-x"
         style={{
           backgroundImage: `url('data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 20" fill="%2392864f"%3E%3Cpolygon points="13,1 25,10 13,19 1,10" /%3E%3C/svg%3E')`,
-          backgroundRepeat: "repeat-x",
         }}
       />
       <div className="h-1 w-full bg-background" />
