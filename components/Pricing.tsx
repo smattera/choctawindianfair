8bfc2ced1d7a3fed491eeff5741c2d00f216fe49
Sonny Mattera <smattera@hotmail.com>
2024-03-28 09:15:11 -0500

    Update Pricing heading color

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -3,7 +3,7 @@ export default function Pricing() {
     <div className="isolate overflow-hidden">
       <div className="mx-auto max-w-7xl px-6 pb-96 pt-24 text-center sm:pt-32 lg:px-8">
         <div className="mx-auto max-w-4xl">
-          <h2 className="text-base font-semibold leading-7 text-amber-600 dark:text-amber-200">
+          <h2 className="text-base font-semibold leading-7 text-amber-700 dark:text-amber-200">
             Pre-Sale Starts June 11, 2024
           </h2>
           <p className="mt-2 text-4xl font-bold tracking-tight text-foreground sm:text-5xl">
